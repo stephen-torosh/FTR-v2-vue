9ca09ed7d89676823e1d6bd1cd2416bf092003b9
stephen-torosh <stephen.torosh@gmail.com>
2025-03-15 10:41:35 +0200

    150325

--- src/components/apps/FileExplorerView.vue
+++ src/components/apps/FileExplorerView.vue
@@ -1,3 +1,33 @@
 <template>
-File Explorer
+<div class="wrapper">
+  <nav>
+    ddds
+  </nav>
+
+  <div class="content">
+    ddd
+  </div>
+</div>
+
 </template>
+
+<style scoped>
+
+.wrapper {
+  display: flex;
+}
+
+nav {
+  display: flex;
+  width: 200px;
+  height: 600px;
+  box-shadow: rgba(0, 0, 0, 0.2) 0px 7px 29px 0px;
+}
+
+.content {
+  display: flex;
+  width: calc(100% - 200px);
+  height: 100%;
+}
+
+</style>
